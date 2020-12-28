34f106d73d0010d33ed3d441f0cd9a8b5cc93e29
Khalid Bello <khalidbello279@gmail.com>
2020-12-28 17:17:00 +0100

    fix: local storage

--- js/main.js
+++ js/main.js
@@ -1,29 +1,76 @@
+let lightMode = localStorage.getItem('lightMode');
+
 let toggle = document.querySelector('.toggle');
-    bgTop = document.querySelector('.bg-top');
-    container = document.querySelector('.container');
-    toggleBall = document.querySelector(".toggle-ball");
-    toggleBallHide = document.querySelector(".toggle-ball-hide"); squares = document.querySelectorAll(".big-square, .small-square");
-    squaresHover = document.querySelector(".big-square:hover");
-    textItems = document.querySelectorAll(".header h4, .handle, .big-square-h4, .small-square-text");
+bgTop = document.querySelector('.bg-top');
+container = document.querySelector('.container');
+toggleBall = document.querySelector(".toggle-ball");
+toggleBallHide = document.querySelector(".toggle-ball-hide");
+squares = document.querySelectorAll(".big-square, .small-square");
+squaresHover = document.querySelector(".big-square:hover");
+textItems = document.querySelectorAll(".header h4, .handle, .big-square-h4, .small-square-text");
 
+const enableLightMode = () => {
 
-toggle.addEventListener('click', function(){
-    
-    bgTop.classList.toggle("bg-top-light");
-    container.classList.toggle("container-light");
-    toggleBallHide.classList.toggle("toggle-show");
-    toggleBall.classList.toggle("toggle-hide");
-    
+    bgTop.classList.add("bg-top-light");
+    container.classList.add("container-light");
+    toggleBallHide.classList.add("toggle-show");
+    toggleBall.classList.add("toggle-hide");
+
     squares.forEach(square => {
-        
-        square.classList.toggle("square-light");
-    })
-    
-    textItems.forEach(textItem => {
-        
-        textItem.classList.toggle("text-light");
+
+        square.classList.add("square-light");
     });
+
+    textItems.forEach(textItem => {
+
+        textItem.classList.add("text-light");
+    });
+
+    toggle.classList.add("bg-image-light");
+
+    localStorage.setItem('lightMode', 'enabled');
+};
+
+
+const disableLightMode = () => {
+
+    bgTop.classList.remove("bg-top-light");
+    container.classList.remove("container-light");
+    toggleBallHide.classList.remove("toggle-show");
+    toggleBall.classList.remove("toggle-hide");
+
+    squares.forEach(square => {
+
+        square.classList.remove("square-light");
+    });
+
+    textItems.forEach(textItem => {
+
+        textItem.classList.remove("text-light");
+    });
+
+    toggle.classList.remove("bg-image-light");
+
+    localStorage.setItem('lightMode', null);
+};
+
+if (lightMode === 'enabled') {
     
-    toggle.classList.toggle("bg-image-light");
-     
-})
+    enableLightMode();
+}
+
+
+toggle.addEventListener('click', function () {
+    
+    lightMode = localStorage.getItem('lightMode')
+
+    if (lightMode !== 'enabled') {
+
+        enableLightMode();
+
+    }else {
+        
+        disableLightMode();
+    }
+
+});
